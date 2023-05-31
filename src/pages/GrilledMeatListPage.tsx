60d75c5f09456bf2bf68abcd156abb914bbad255
Trust60 <dmerteyhrabovetski@gmail.com>
2023-05-31 23:17:21 +0300

    Get data grilled meat products from database

--- src/pages/GrilledMeatListPage.tsx
+++ src/pages/GrilledMeatListPage.tsx
@@ -1,39 +1,32 @@
-import { useEffect, useState } from 'react';
-import database from '../components/Database/MeatProducts.json';
 import styles from '../styles/modules/GrilledMeatPage.module.scss';
 import Header from '../components/header/Header';
 import GrilledMeatCard from '../components/grilledMeatCard/GrilledMeatCard';
+import { useGetProductsQuery } from '../store/services/shopApi';
+import Loader from '../components/loader/PageLoader';
 const GrilledMeatListPage = () => {
-	const [products, setProducts] = useState([]);
-	useEffect(() => {
-		// Загрузка данных из файла базы данных
-		const fetchData = async () => {
-			try {
-				// Здесь можно использовать логику для загрузки данных, например, через fetch или axios
-				// В данном примере мы просто симулируем загрузку данных из локального файла
-				const response = await new Promise((resolve) =>
-					setTimeout(() => resolve(database.products), 1000),
-				);
-
-				setProducts(response);
-			} catch (error) {
-				console.error('Error fetching data:', error);
-			}
-		};
-
-		fetchData();
-	}, []);
+	const { data, isLoading } = useGetProductsQuery();
+	console.log(data);
 
 	return (
 		<>
 			<Header />
-			<div className={styles.container}>
-				{products.map((product) => (
-					<div key={product.id}>
-						<GrilledMeatCard title={product.title} image={product.image} price={product.price} />
-					</div>
-				))}
-			</div>
+			{isLoading ? (
+				<div className={styles.loader}>
+					<Loader />
+				</div>
+			) : (
+				<div className={styles.container}>
+					{data?.map((products) => (
+						<div key={products.id}>
+							<GrilledMeatCard
+								title={products.title}
+								image={products.image}
+								price={products.price}
+							/>
+						</div>
+					))}
+				</div>
+			)}
 		</>
 	);
 };
